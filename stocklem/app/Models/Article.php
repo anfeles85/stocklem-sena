e05ea9930474ffa26bcdf9b503bfa9c29185a2ee
Juan_Diego <juan3312orrego@gmail.com>
2025-06-12 12:34:04 -0500

    Models P1
    Se inician los primeros modelos sin llaves foraneas

---
+++ stocklem/app/Models/Article.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    protected $table = 'article';
+
+    protected $fillable = [
+        'name',
+        'quantity',
+        'photo',
+        'technical_sheet'
+    ];
+
+    public function presentation() {
+        return $this->belongsTo(Presentation::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function supplier() {
+        return $this->belongsTo(Supplier::class);
+    }
+}
