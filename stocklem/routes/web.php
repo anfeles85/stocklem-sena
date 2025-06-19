e315d2502f03508a88321a7546a279ef8d886eb3
JuanJo Arenas <dragonex579@gmail.com>
2025-06-19 12:13:16 -0500

    Modificacion Rutas
    se añadieron cambios en las rutas

--- stocklem/routes/web.php
+++ stocklem/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix()->group(function(){
+Route::prefix('category')->group(function(){
     Route::get('/index', [CategoryController::class, 'index'])->name('category.index');
     Route::get('/create', [CategoryController::class, 'create'])->name('category.create');
     Route::get('/store', [CategoryController::class, 'store'])->name('category.store');
@@ -31,7 +31,7 @@ Route::prefix()->group(function(){
     Route::get('/destroy/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
 });
 
-Route::prefix()->group(function(){
+Route::prefix('person')->group(function(){
     Route::get('/index', [PersonController::class, 'index'])->name('person.index');
     Route::get('/create', [PersonController::class, 'create'])->name('person.create');
     Route::get('/store', [PersonController::class, 'store'])->name('person.store');
@@ -40,7 +40,7 @@ Route::prefix()->group(function(){
     Route::get('/destroy/{id}', [PersonController::class, 'destroy'])->name('person.destroy');
 });
 
-Route::prefix()->group(function(){
+Route::prefix('presentation')->group(function(){
     Route::get('/index', [PresentationController::class, 'index'])->name('presentation.index');
     Route::get('/create', [PresentationController::class, 'create'])->name('presentation.create');
     Route::get('/store', [PresentationController::class, 'store'])->name('presentation.store');
@@ -49,7 +49,7 @@ Route::prefix()->group(function(){
     Route::get('/destroy/{id}', [PresentationController::class, 'destroy'])->name('presentation.destroy');
 });
 
-    Route::prefix()->group(function(){
+    Route::prefix('supplier')->group(function(){
     Route::get('/index', [SupplierController::class, 'index'])->name('supplier.index');
     Route::get('/create', [SupplierController::class, 'create'])->name('supplier.create');
     Route::get('/store', [SupplierController::class, 'store'])->name('supplier.store');
@@ -58,7 +58,7 @@ Route::prefix()->group(function(){
     Route::get('/destroy/{id}', [SupplierController::class, 'destroy'])->name('supplier.destroy');
 });
 
-    Route::prefix()->group(function(){
+    Route::prefix('unit')->group(function(){
     Route::get('/index', [UnitController::class, 'index'])->name('unit.index');
     Route::get('/create', [UnitController::class, 'create'])->name('unit.create');
     Route::get('/store', [UnitController::class, 'store'])->name('unit.store');
