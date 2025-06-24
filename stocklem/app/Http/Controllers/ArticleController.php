1303141932f3851387a80eae8866c32d2a9818ca
JuanJo Arenas <dragonex579@gmail.com>
2025-06-24 08:59:27 -0500

    Controllers FK P1
    se crea el controlador de articulo

---
+++ stocklem/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ArticleController extends Controller
+{
+
+    private $rules = [
+        'name' => 'required|string|min:3|max:100',
+        'quantity' => 'required|numeric|min:1|max:9999999999',
+        'photo' => 'max:255',
+        'technical_sheet' => 'max:255',
+        'presentation_id' => 'max:9999999999999999999',
+        'category_id' => 'max:9999999999999999999',
+        'supplier_id' => 'max:9999999999999999999'
+    ];
+
+    private $traductionAttributes = [
+        'name' => 'nombre',
+        'quantity' => 'cantidad',
+        'photo' => 'foto',
+        'technical_sheet' => 'ficha técnica',
+        'presentation_id' => 'presentación',
+        'category_id' => 'categoría',
+        'supplier_id' => 'proveedor'
+
+    ];
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articles = Article::all();
+        return view('article.index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('article.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), $this->rules);
+        $validator->setAttributeNames($this->traductionAttributes);
+        if($validator->fails())
+        {
+            $errors = $validator->errors();
+            return redirect()->route('article.create')->withInput()->withErrors($errors);
+        }
+        $article = Article::create($request->all());
+        return redirect()->route('article.index')->with('success', 'Artículo creado exitosamente');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $article = Article::find($id);
+        if($article)
+        {
+            return view('article.edit', compact('article'));
+        }
+        else
+        {
+            return redirect()->route('article.index')->with('Error', 'No se encontró el artículo');
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validator = Validator::make($request->all(), $this->rules);
+        $validator->setAttributeNames($this->traductionAttributes);
+        if($validator->fails())
+        {
+            $errors = $validator->errors();
+            return redirect()->route('article.edit', $id)->withInput()->withErrors($errors);
+        }
+        $article = Article::find($id);
+        if($article){
+            $article->update($request->all());
+            return redirect()->route('article.index')->with('success', '¡Artículo actualizado correctamente!');
+        }
+        return redirect()->route('article.index')->with('error', 'Ha ocurrido un problema al actualizar el artículo.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $article = Article::find($id);
+        if($article)
+        {
+            $article->delete();
+            return redirect()->route('article.index')->with('success', '¡Artículo eliminado correctamente!');
+        }
+        else
+        {
+            return redirect()->route('article.index')->with('error', 'Ha ocurrido un problema al eliminar el artículo.');
+        }
+    }
+}
