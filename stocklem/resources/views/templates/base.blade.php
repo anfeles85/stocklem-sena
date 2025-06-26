02400edcf4e455566a5c3241c2361296080ee586
daniel ortiz  2003 <danielortizmanzanares@gmail.com>
2025-06-26 12:29:34 -0500

    Implementar SweetAlert2 y table_data
    se implemto SweetAlert2 en la base y en el index de Unit

--- stocklem/resources/views/templates/base.blade.php
+++ stocklem/resources/views/templates/base.blade.php
@@ -64,25 +64,13 @@
 
   @if(session('success'))
   <script>
-    Swal.fire({
-      icon: 'success',
-      title: '¡OK!',
-      text: '{{ session("success") }}',
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'OK'
-    });
+    showSuccess('{{ session("success") }}');
   </script>
   @endif
 
   @if(session('error'))
   <script>
-    Swal.fire({
-      icon: 'error',
-      title: '¡Error!',
-      text: '{{ session("error") }}',
-      confirmButtonColor: '#d33',
-      confirmButtonText: 'OK'
-    });
+    showError('{{ session("error") }}');
   </script>
   @endif
 
