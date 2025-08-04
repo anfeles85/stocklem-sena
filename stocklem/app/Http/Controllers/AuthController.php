59c20df74a0ce21eb63f200ecf744ef87cdbb658
JuanJo Arenas <dragonex579@gmail.com>
2025-08-04 14:19:11 -0500

    Metodos AuthController
    se implementan los métodos de la autenticación de los usuarios

--- stocklem/app/Http/Controllers/AuthController.php
+++ stocklem/app/Http/Controllers/AuthController.php
@@ -3,8 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    /**
+     * Show the login form.
+     */
+    public function index()
+    {
+        if(Auth::check()) {
+            return redirect()->route('index');
+        }
+        return view('login.form');
+    }
+
+    /**
+     * Login the user
+     */
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('index');
+        }
+
+        return back()->withErrors([
+            'email' => 'Credenciales incorrectas',
+        ])->onlyInput('email');
+    }
+
+    /**
+     * Logout the user
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login.form');
+    }
 }
