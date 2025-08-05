c456927ca148891b10a7beb08306102145397639
Juan_Diego <juan3312orrego@gmail.com>
2025-08-05 08:24:11 -0500

    FIX: AuthController - merge errors

--- stocklem/app/Http/Controllers/AuthController.php
+++ stocklem/app/Http/Controllers/AuthController.php
@@ -8,23 +8,6 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     /**
-<<<<<<< HEAD
-     * Show the login form.
-     */
-    public function index()
-    {
-        if(Auth::check()) {
-            return redirect()->route('index');
-        }
-        return view('auth.form');
-    }
-
-    /**
-     * Login the user
-     */
-    public function login(Request $request)
-    {
-=======
      * Display a listing of the resource.
      */
     public function index()
@@ -40,41 +23,22 @@ class AuthController extends Controller
      * Login de usuarios
      */
     public function login(Request $request){
->>>>>>> G4
         $credentials = $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-<<<<<<< HEAD
-        if(Auth::attempt($credentials)) {
-=======
         if (Auth::attempt($credentials)){
->>>>>>> G4
             $request->session()->regenerate();
             return redirect()->intended('index');
         }
 
         return back()->withErrors([
-<<<<<<< HEAD
-            'email' => 'Credenciales incorrectas',
-=======
             'email' => 'Estas credenciales no coinciden con nuestros registros.',
->>>>>>> G4
         ])->onlyInput('email');
     }
 
     /**
-<<<<<<< HEAD
-     * Logout the user
-     */
-    public function logout(Request $request)
-    {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect()->route('auth.login.form');
-=======
      * Cerrar la sesión del usuario
      */
     public function logout(Request $request){
@@ -130,6 +94,5 @@ class AuthController extends Controller
     public function destroy(string $id)
     {
         //
->>>>>>> G4
     }
 }
